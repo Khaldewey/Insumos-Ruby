40aac9d057eef56a1b7e2d917b162debf4dab08c
Israel <israel_alves77@hotmail.com>
2024-02-05 19:34:21 -0300

    outra quebrada de maldição, entrada e saída de dados

--- teste.rb
+++ teste.rb
@@ -1,2 +1,18 @@
-puts "Hello World !"
-puts "Maldição quebrada com sucesso kkk"
+# puts "Hello World !"
+# puts "Maldição quebrada com sucesso kkk" 
+
+# print "Digite o seu nome: "
+
+# nome = gets.chomp 
+
+# puts "Oi #{nome} !" 
+
+print "Digite um número ae vagabundo! "
+num1 = gets.chomp.to_i
+
+print "Digite outro número vagabundo! "
+num2 = gets.chomp.to_i 
+
+soma = num1 + num2 
+
+puts "O resultado foi #{soma} !"
